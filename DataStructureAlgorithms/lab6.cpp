1cf35e8d33356684d8385e106bcb12d58c79c223
Aki78 <kmorooka78@gmail.com>
2024-01-27 15:10:02 +0200

    refactor 6A setup

--- DataStructureAlgorithms/lab6.cpp
+++ DataStructureAlgorithms/lab6.cpp
@@ -1,13 +1,86 @@
 /* dynlist.cpp -- Dynamic linked linear list */
 
 #include <iostream>
-#include "dynlist.h"
 
 using namespace std;
 
+
+// Interface of list
+template <class T>
+class LinkedList {
+private:
+	// definition of the list node class
+	class Node {
+	public:
+		T _value;	// data
+		Node *_pNext;	// pointer to the next node
+		Node() : _pNext(NULL) {}
+		Node(T val) : _value(val), _pNext(NULL) {}
+		Node(T val, Node* next) : _value(val), _pNext(next) {}
+	};
+
+	Node *first;
+	Node *last;
+
+public:
+
+
+
+
+	LinkedList() {
+		first = NULL;
+		last = NULL;
+	}
+
+	~LinkedList() {
+		Node *aux1, *aux2;
+
+		aux1 = first;
+		while (aux1 != NULL) {
+			aux2 = aux1->_pNext;
+			delete aux1;
+			printf("\nDeleted"); //for testing purposes
+			aux1 = aux2;
+		}
+	}
+
+	LinkedList<T>& insert_to_end(T data) {
+		Node *newnode = new Node(data);
+		if (first == NULL)
+			first = newnode;		//first node
+		else
+			last->_pNext = newnode;	//not first node
+		last = newnode;
+
+		return *this;
+	}
+
+	void print(ostream &out) const {
+		Node *what;
+
+		out << "\nList:";
+		what = first;
+		while (what != NULL) {
+			out << " " << what->_value << " ";
+			what = what->_pNext;
+		}
+		out << endl;
+	}
+};
+
+// Implementation of list
+
+template <class T>
+ostream &operator<<(ostream &out,  LinkedList<T> const &data) {
+	data.print(out);
+	return out;
+}
+
+
+
 // Application
 int main (void)  {
-    LinkedList<char> list;
+	LinkedList<char> list;
 
 	list.insert_to_end('a').insert_to_end('b').insert_to_end('c').insert_to_end('d');
 
