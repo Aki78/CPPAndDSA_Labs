e9f0216e45b49e93e76426402b724d3bc009b709
Aki78 <kmorooka78@gmail.com>
2024-01-31 18:43:57 +0200

    everything working in lab7 except find

--- DataStructureAlgorithms/lab7.cpp
+++ DataStructureAlgorithms/lab7.cpp
@@ -74,29 +74,70 @@ cout << "test "<< endl;
 		out << endl;
 	}
 
+	void print() const {
+		Node *what;
+
+		cout << "\nList:";
+		what = first;
+		while (what != NULL) {
+			cout << " " << what->_value << " ";
+			what = what->_pNext;
+		}
+		cout << endl;
+	}
+
 
 	void delete_first(){
-		Node *aux1, *aux2;
-
-		aux1 = first;
-		aux2 = aux1->_pNext;
-		delete aux1;
-		aux1 = aux2;
+		if (first != NULL){
+			Node *aux1, *aux2;
+			aux1 = first;
+			aux2 = aux1->_pNext;
+			delete[] aux1;
+			aux1 = aux2;
+		}
 
 	}
 
 	void delete_last(){
-	
+
+		if (first != NULL){
+			Node *aux1; 
+			Node  aux2;
+			
+			aux1 = first;
+
+			while (aux1 -> _pNext != NULL) {
+				aux2 = *aux1;
+				aux1 = aux1->_pNext;
+			}
+			delete[] aux1-> _pNext;
+			aux2._pNext = NULL;
+		}
+			
 	}
 
 	bool find_pos(T item, int &pos){
+		if (first != NULL){
+			Node *aux1; 
+			Node  aux2;
+			
+			aux1 = first;
 
+			while (aux1 -> _pNext != NULL) {
+				aux2 = *aux1;
+				aux1 = aux1->_pNext;
+			}
+			delete[] aux1-> _pNext;
+			aux2._pNext = NULL;
+		}
+			
+		return false;
 	}
+
 
 
 };
 
-// Implementation of list
 
 template <class T>
 ostream &operator<<(ostream &out,  LinkedList<T> const &data) {
@@ -105,88 +146,8 @@ ostream &operator<<(ostream &out,  LinkedList<T> const &data) {
 }
 
 
-//====PartB=======================================================
 
 
-class NodeData {
-
-	friend ostream& operator<< (ostream& stream, const NodeData *p);
-
-	public:
-	virtual ostream& print(ostream &stream) const = 0;
-	virtual bool operator<(NodeData *) = 0;
-
-};
-
-
-ostream& operator<< (ostream& stream, const NodeData *p) {
-	return p->print(stream);
-}
-
-
-class Complex: public NodeData{
-private:
-
-	double x;
-	double y;
-
-
-public:
-
-	Complex(double x0=0, double y0=0):x(x0),y(y0){}
-
-	virtual ostream& print(ostream &stream) const override {
-		stream << x << " + " << y << "i";
-		return stream;
-	}
-
-
-
-	virtual bool operator<(NodeData *node) override {
-		Complex *complexNode = dynamic_cast<Complex*>(node);
-//		Complex *complexNode = node; // Did not work...
-		if (!complexNode) {
-			return false; 
-		}
-		
-		return (x * x + y * y) < (complexNode->x * complexNode->x + complexNode->y * complexNode->y);
-	}
-
-
-	friend ostream& operator<< (ostream& stream, const Complex &c) {
-		return c.print(stream);
-	}
-
-
-};
-
-
-class String : public NodeData {
-private:
-	std::string value;
-
-public:
-	String(const std::string &val) : value(val) {}
-
-	virtual ostream& print(ostream &stream) const override {
-		stream << value;
-		return stream;
-	}
-
-	virtual bool operator<(NodeData *other) override {
-		String *stringOther = dynamic_cast<String*>(other);
-		if (!stringOther) {
-			return false; // or handle differently
-		}
-		return value < stringOther->value;
-	}
-};
-
-
-
-
-
-//================================================================
 // Application
 int main ()  {
 
