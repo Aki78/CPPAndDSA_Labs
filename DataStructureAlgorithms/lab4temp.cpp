e975c137309cd3e53fa00d5b6f479b5f05b8fab3
Aki78 <kmorooka78@gmail.com>
2024-02-01 02:49:19 +0200

    updated lab4 to hand in

--- DataStructureAlgorithms/lab4temp.cpp
+++ DataStructureAlgorithms/lab4temp.cpp
@@ -55,126 +55,45 @@ public:
 };
 
 
-//class CalculatorA {
-//
-//	private:
-//		string input, temp0, temp1;
-//		Stack<string> stack;
-//		
-//
-//	bool add(){
-//
-//		if(!stack.pop(temp0)){
-//			stack.push(temp0);
-//			return false;
-//		}
-//		if(!stack.pop(temp1)){
-//			stack.push(temp0);
-//			return false;
-//		}
-//		stack.push(to_string(stod(temp0) + stod(temp1)));
-//
-//		return true;
-//
-//	}
-//
-//
-//	bool subtract(){
-//
-//		if(!stack.pop(temp0)){
-//			stack.push(temp0);
-//			return false;
-//		}
-//		if(!stack.pop(temp1)){
-//			stack.push(temp0);
-//			return false;
-//		}
-//		stack.push(to_string(stod(temp1) - stod(temp0)));
-//
-//		return true;
-//
-//	}
-//
-//	void printTop(){
-//
-//		stack.pop(temp0) ;
-//		cout << "Top Value is: " << temp0 << endl;
-//		stack.push(temp0);
-//
-//	}
-//
-//	void operateOnStack(){
-//
-//		string op;
-//		stack.pop(op);	
-//		
-//		switch (op[0]) {
-//			case '+':
-//				add();
-//				break;
-//			case '-':
-//				subtract();
-//				break;
-//			case '=':
-//				printTop();
-//				break;
-//			case 'Q':
-//				stack.print();
-//				break;
-//			default:
-//				cout << "Invalid operator" << endl;
-//				break;
-//		}
-//	}
-//
-//	public:
-//
-//	CalculatorA() {} 
-//	void run() {
-//		cout << "Enter a number or an operator (+, -, =)" << endl;
-//		while (true) {
-//
-//			getline(cin, input);
-//			
-//
-//			if (getInputType(input) == NUM) {
-//				stack.push(input);
-//
-//			} else if (getInputType(input) == OPE) {
-//				
-//				stack.push(input);
-//				operateOnStack();
-//
-//			} else {
-//
-//				cout << "Incorrect" << endl;
-//			}
-//		}
-//
-//	}
-//	bool getInputType(const string& input) {
-//
-//		stringstream ss(input);
-//		double num;
-//		if (ss >> num && ss.eof()) {
-//			return NUM;
-//		}
-//
-//		if (input == "+" || input == "-" || input == "=" || input == "Q") {
-//			return OPE;
-//		}
-//
-//		return BAD;
-//	}
-//};
-
-
-class CalculatorB1 {
+class CalculatorA {
 
 	private:
+		string input, temp0, temp1;
 		Stack<string> stack;
-		string temp0;
-		string input;
+		
+
+	bool add(){
+
+		if(!stack.pop(temp0)){
+			stack.push(temp0);
+			return false;
+		}
+		if(!stack.pop(temp1)){
+			stack.push(temp0);
+			return false;
+		}
+		stack.push(to_string(stod(temp0) + stod(temp1)));
+
+		return true;
+
+	}
+
+
+	bool subtract(){
+
+		if(!stack.pop(temp0)){
+			stack.push(temp0);
+			return false;
+		}
+		if(!stack.pop(temp1)){
+			stack.push(temp0);
+			return false;
+		}
+		stack.push(to_string(stod(temp1) - stod(temp0)));
+
+		return true;
+
+	}
 
 	void printTop(){
 
@@ -184,46 +103,55 @@ class CalculatorB1 {
 
 	}
 
-	public:
+	void operateOnStack(){
 
-	CalculatorB1() {} 
-	int run() {
-		cout << "Give an infix equation at one line" << endl;
-		getline(cin, input);
-
-		vector<string> stackList = stringToCharVector(input);
-		Stack<string> opeStack;
-		string temp_char;
-
-		for(string c: stackList ) {
-
-			if (getInputType(c) == NUM) stack.push(c);
-			else if (getInputType(c) == OPE) opeStack.push(c);
-			else return -1;
-			
-		}
+		string op;
+		stack.pop(op);	
 		
-		while(opeStack.pop(temp_char)) { stack.push(temp_char);}
-		stack.print();
-		printTop();
-
-		return 0;
+		switch (op[0]) {
+			case '+':
+				add();
+				break;
+			case '-':
+				subtract();
+				break;
+			case '=':
+				printTop();
+				break;
+			case 'Q':
+				stack.print();
+				break;
+			default:
+				cout << "Invalid operator" << endl;
+				break;
+		}
 	}
 
+	public:
 
-	vector<string> stringToCharVector(const string& str) {
-		vector<string> charVec;
+	CalculatorA() {} 
+	void run() {
+		cout << "Enter a number or an operator (+, -, =)" << endl;
+		while (true) {
 
+			getline(cin, input);
+			
 
-		for (char c : str) {
-			if (c != ' ') {  
-				charVec.push_back(std::string(1, c));  
+			if (getInputType(input) == NUM) {
+				stack.push(input);
+
+			} else if (getInputType(input) == OPE) {
+				
+				stack.push(input);
+				operateOnStack();
+
+			} else {
+
+				cout << "Incorrect" << endl;
 			}
 		}
 
-		return charVec;
 	}
-
 	bool getInputType(const string& input) {
 
 		stringstream ss(input);
@@ -232,54 +160,24 @@ class CalculatorB1 {
 			return NUM;
 		}
 
-		if (input == "+" || input == "-" || input == "*" || input == "/" ||  input == "=" || input == "Q") {
+		if (input == "+" || input == "-" || input == "=" || input == "Q") {
 			return OPE;
 		}
 
 		return BAD;
 	}
-
-	void infixTopostfix(char *infix, char *postfix){
-
-		for(char c : infix) {
-			if(isdigit(c)) {
-				postfix += c;
-			} else if(c == '+' || c == '*') {
-				while(!stack.empty() && precedence(c) <= precedence(stack.top())) {
-					postfix += stack.top();
-					stack.pop();
-				}
-				stack.push(c);
-			}
-		}
-
-		while(!stack.empty()) {
-			postfix += stack.top();
-			stack.pop();
-		}
-
-	}
-
-
-	int precedence(char op) {
-		if(op == '*') return 2;
-		if(op == '+') return 1;
-		return 0;
-	}
 };
+
+
 
 
 int main() {
 
 // Part A
 
-//	CalculatorA calc;
-//	calc.run();
-
-// Part B1
-
-	CalculatorB1 calc;
+	CalculatorA calc;
 	calc.run();
+
 
 
 	return 0;
