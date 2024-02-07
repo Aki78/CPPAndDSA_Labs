e5a3ddf8d6dc7baa38f8f5c114125886f9bff4fe
Aki78 <kmorooka78@gmail.com>
2024-02-07 21:05:22 +0200

    done lab12 A

--- DataStructureAlgorithms/lab12.cpp
+++ DataStructureAlgorithms/lab12.cpp
@@ -1,6 +1,9 @@
 #include <complex>
 #include <numeric>
 #include <iostream>
+#include <vector>
+#include <chrono>
+#include <math.h>
 
 using namespace std;
 
@@ -34,21 +37,63 @@ void fft(int n, cx x[]) {
 	delete[] xo;
 }
 
+double get_average(vector<double> my_vec){
+	double accum = 0;
+	double count = 0;
+	for(auto v: my_vec){
+		accum += v;
+		count += 1;
+	}
+
+	return  accum / count;
+}
+
+
+double get_standdiv(vector<double> my_vec, double size, double ave){
+	double accum = 0;
+	double count = 0;
+	for(auto v: my_vec){
+		accum += (v - ave)*(v - ave);
+	}
+
+	return  sqrt(accum / size);
+}
+
+
 
 int main() {
-	const int N = 8;
-	cx input[N];
+
+	vector<double> average_me;
+
+	int N = 8;
+	for (int n = 0; n < 15; n++){ // segfault over 15, but why?
+	N *=2;
+	complex<double> input[N];
 
 	// create the test input signal sequence
 	for (int i = 0; i < N; i++)
-		input[i] = cx(i, -i);  // linear ramp for real part, negative ramp for imaginary part
+		input[i] = complex<double>(i, -i);  // linear ramp for real part, negative ramp for imaginary part
 
-	fft(N, input);
+	vector <double> avvec;
+	for (int a = 0; a < 30; a++){ // measuring 30 times for statistics.
+		auto start = chrono::high_resolution_clock::now();
+
+		fft(N, input);
+
+		auto stop = chrono::high_resolution_clock::now();
+		auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+		double duration_double = static_cast<double>(duration.count());
+		avvec.push_back(duration_double);
+	}
+	double ave_val = get_average(avvec);
+	cout << "Time taken by fft at N=" << N << " Average microseconds: "
+		 << (int) ave_val << "±" << (int) get_standdiv(avvec, avvec.size(), ave_val) <<  endl;
+
 
 	// display the results
-	for (cx &c: input)
-		cout << c << endl;
+//	for (const auto& c: input)
+//		cout << c << endl;
 
-	system("PAUSE");
+	}
 	return EXIT_SUCCESS;
 }
